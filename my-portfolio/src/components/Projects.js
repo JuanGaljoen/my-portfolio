3a7282e5f75147c73d892eeb187685709471d14e
Juan Viljoen <juangaljoen@gmail.com>
2023-08-04 13:30:52 +0200

    added new project

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,97 +1,6 @@
-// import cartPic from "../images/cart.jpg"
-// import memoryPic from "../images/memory.jpg"
-// import loadingGif from "../images/loading.gif"
-// import weatherPic from "../images/weather.jpg"
-// import todoPic from "../images/todo.jpg"
-// import inventoryPic from "../images/inventory.jpg"
-// import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-// import { ProjectCard } from "./ProjectCard";
-
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
-
-// export const Projects = () => {
-
-//   const projects = [
-//     {
-//       title: "Functional e-commerce cart",
-//       description: "One of my first more advanced JavaScript projects",
-//       imgUrl: cartPic,
-//       link: "https://github.com/JuanBiljoen/e-commerce-Shopping-cart"
-//     },
-//     {
-//       title: "Memory game",
-//       description: "a Card memory game built with React.",
-//       imgUrl: memoryPic,
-//       link: "https://memory-app-ten.vercel.app/"
-//     },
-//     {
-//       title: "Weather app",
-//       description: "Weather app built with React",
-//       imgUrl: weatherPic ,
-//       link: "https://weather-sand-sigma.vercel.app/"
-//     },
-//     {
-//       title: "Todo app (Full Stack)",
-//       description: "Todo-List app built with MERN stack",
-//       imgUrl: todoPic,
-//       link: "https://github.com/JuanBiljoen/mern-todo"
-//     },
-//     {
-//       title: "Inventory manager (Full stack MERN application)",
-//       description: "Inventory management system",
-//       imgUrl: inventoryPic,
-//       link: "https://github.com/JuanBiljoen/inventory"
-//     },
-//     {
-//       title: "Next Project",
-//       description: "Loading...",
-//       imgUrl: loadingGif,
-//     },
-
-
-//   ];
-
-//   return (
-//     <section className="project" id="projects">
-//       <Container>
-//         <Row>
-//           <Col size={12}>
-//             <TrackVisibility>
-//               {({ isVisible }) =>
-//                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-//                   <h2>Projects</h2>
-//                   <p>Here are a couple of projects I have worked on thus far.</p>
-
-//                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-//                     <Tab.Pane eventKey="first">
-//                       <Row>
-//                         {
-//                           projects.map((project, index) => {
-//                             return (
-//                               <ProjectCard
-//                                 key={index}
-//                                 {...project}
-//                               />
-//                             )
-//                           })
-//                         }
-//                       </Row>
-//                     </Tab.Pane>
-//                   </Tab.Content>
-//                 </div>}
-//             </TrackVisibility>
-//           </Col>
-//         </Row>
-//       </Container>
-
-//     </section>
-//   )
-// }
-
 import cartPic from "../images/cart.jpg"
 import memoryPic from "../images/memory.jpg"
-import weatherPic from "../images/weather.jpg"
+import portfolioPic from "../images/resume.jpg"
 import todoPic from "../images/todo.jpg"
 import inventoryPic from "../images/inventory.jpg"
 import carsPic from "../images/cars.jpg"
@@ -104,10 +13,10 @@ import 'animate.css';
 export const Projects = () => {
   const projects = [
     {
-      title: "e-commerce cart",
-      description: "Vanilla JavaScript shopping cart, web-based application that allows users to select items from an online store, add them to a shopping cart, and proceed to checkout where you can choose delivery and add a promo code. The shopping cart is designed using HTML, CSS, and JavaScript.",
-      imgUrl: cartPic,
-      link: "https://github.com/JuanBiljoen/e-commerce-Shopping-cart"
+      title: "AWS Cloud Resume",
+      description: "This portfolio is bootstrapped with React and hosted on AWS using S3, Cloudfront, Route53, Lambda, API Gateway and DynamoDB. CI/CD is set up with Github Actions for the frontend and Terraform for the backend",
+      imgUrl: portfolioPic ,
+      link: "https://github.com/JuanBiljoen/cloud-resume/blob/main/my-portfolio/README.md"
     },
     {
       title: "Memory game",
@@ -116,10 +25,10 @@ export const Projects = () => {
       link: "https://memory-app-ten.vercel.app/"
     },
     {
-      title: "Weather app",
-      description: "The React-based weather app fetches APIs to display the weather and photo of a city searched by the user. The app uses third-party APIs like OpenWeatherMap and Unsplash to fetch real-time weather data and photos respectively. The app features an easy-to-use interface that allows users to search for any city and view the current weather conditions along with a relevant photo of the location.",
-      imgUrl: weatherPic ,
-      link: "https://weather-sand-sigma.vercel.app/"
+      title: "e-commerce cart",
+      description: "Vanilla JavaScript shopping cart, web-based application that allows users to select items from an online store, add them to a shopping cart, and proceed to checkout where you can choose delivery and add a promo code. The shopping cart is designed using HTML, CSS, and JavaScript.",
+      imgUrl: cartPic,
+      link: "https://github.com/JuanBiljoen/e-commerce-Shopping-cart"
     },
     { 
       title: "Cars Manager",
