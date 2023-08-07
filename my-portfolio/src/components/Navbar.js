c2def7f49bd3a5c6d34dbf87cf478d030ffb0191
Juan Viljoen <juangaljoen@gmail.com>
2023-08-07 12:02:46 +0200

    added comments to code

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -4,9 +4,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
 
 function NavBar() {
+    // State variables to track the active link and the scroll position
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
+    // useEffect hook to add scroll event listener and update the scroll position
     useEffect(() => {
         const onScroll = () => {
             if (window.scrollY > 50) {
@@ -16,35 +18,39 @@ function NavBar() {
             }
         }
 
+        // Add the scroll event listener when the component mounts
         window.addEventListener("scroll", onScroll)
 
+        // Clean up the scroll event listener when the component unmounts
         return () => window.removeEventListener("scroll", onScroll)
     }, [])
 
+    // Function to update the active link in the navigation bar
     const onUpdateActiveLink = (value) => {
-        setActiveLink(value)
+        setActiveLink(value);
     }
 
     return (
+        // Navigation bar component with Bootstrap styling
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
-
-
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
+                        {/* Navigation links with conditional class 'active' based on activeLink state */}
                         <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
                         <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
                         <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
                         <Nav.Link href="#aboutme" className={activeLink === 'aboutme' ? 'active navbar-link' : "navbar-link"} onClick={() => onUpdateActiveLink("aboutme")}>About Me</Nav.Link>
-
                         <Nav.Link href="#connect" className={activeLink === 'connect' ? 'active navbar-link' : "navbar-link"} onClick={() => onUpdateActiveLink("connect")}>Let's Connect</Nav.Link>
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
+                            {/* Social media icons */}
                             <a href="https://juancodes.blogspot.com/"><img src={"https://www.iconpacks.net/icons/1/free-blogger-icon-107-thumb.png"} alt="" /></a>
                             <a href="https://www.linkedin.com/in/juan-viljoen-409895245/"><img src={"https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png"} alt="" /></a>
                             <a href="https://github.com/JuanBiljoen"><img src={"https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg"} alt="" /></a>
                         </div>
+                        {/* 'Let's Connect' button */}
                         <button href="#connect" className='vvd' onClick={() => onUpdateActiveLink("connect")}><span>Let's Connect</span></button>
                     </span>
                 </Navbar.Collapse>
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -9,45 +9,52 @@ import { ProjectCard } from "./ProjectCard";
 import TrackVisibility from 'react-on-screen';
 import 'animate.css';
 
-
 export const Projects = () => {
+  // Array of project objects with title, description, image URL, and link
   const projects = [
+    // Project 1: AWS Cloud Resume
     {
       title: "AWS Cloud Resume",
-      description: "This portfolio is bootstrapped with React and hosted on AWS using S3, Cloudfront, Route53, Lambda, API Gateway and DynamoDB. CI/CD is set up with Github Actions for the frontend and Terraform for the backend",
-      imgUrl: portfolioPic ,
+      description: "This portfolio is bootstrapped with React and hosted on AWS using S3, Cloudfront, Route53, Lambda, API Gateway, and DynamoDB. CI/CD is set up with Github Actions for the frontend and Terraform for the backend.",
+      imgUrl: portfolioPic,
       link: "https://github.com/JuanBiljoen/cloud-resume/blob/main/my-portfolio/README.md"
     },
+    // Project 2: Memory game
     {
       title: "Memory game",
       description: "The React-based match card game is a fun and interactive game that challenges users to match pairs of cards. The game features a deck of cards with different images, and the user's objective is to match all the cards in the shortest time possible.",
       imgUrl: memoryPic,
       link: "https://memory-app-ten.vercel.app/"
     },
+    // Project 3: E-commerce cart
     {
       title: "e-commerce cart",
       description: "Vanilla JavaScript shopping cart, web-based application that allows users to select items from an online store, add them to a shopping cart, and proceed to checkout where you can choose delivery and add a promo code. The shopping cart is designed using HTML, CSS, and JavaScript.",
       imgUrl: cartPic,
       link: "https://github.com/JuanBiljoen/e-commerce-Shopping-cart"
     },
-    { 
+    // Project 4: Cars Manager
+    {
       title: "Cars Manager",
       description: "This application includes both the frontend created with ReactJS and the backend part created with ExpressJS and the database setup with MongoDB. The data interacts with the DB using CRUD operations.",
-      imgUrl: carsPic ,
+      imgUrl: carsPic,
       link: "https://github.com/JuanBiljoen/car-manager#readme"
     },
+    // Project 5: Todo app (Full Stack)
     {
       title: "Todo app (Full Stack)",
       description: "The MERN stack to-do app enables users to create an account, add to-do items, and edit or delete them later. The app features secure authentication, options to mark tasks as complete or pending, and an intuitive user interface. It is a powerful and efficient solution for managing tasks.",
       imgUrl: todoPic,
       link: "https://github.com/JuanBiljoen/mern-todo"
     },
+    // Project 6: Inventory manager (Full stack MERN application)
     {
       title: "Inventory manager (Full stack MERN application)",
       description: "The inventory management app built using the MERN stack has admin and end-user features, uses JWT authentication, and is designed for businesses to manage their inventory efficiently and securely. The app allows admins to manage products, orders, shipments, and user access levels. End-users can browse products, add to cart, track orders, and view order history.",
       imgUrl: inventoryPic,
       link: "https://github.com/JuanBiljoen/inventory"
     },
+    // Next Project: Placeholder for future projects
     // {
     //   title: "Next Project",
     //   description: "Loading...",
@@ -57,32 +64,33 @@ export const Projects = () => {
 
   return (
     <section className="project" id="projects">
-    <Container>
-      <Row>
-        <Col size={12}>
-          <TrackVisibility>
-            {({ isVisible }) =>
-              <div className={isVisible ? "fadeIn" : ""}>
-                <h2>Projects</h2>
-                <p>Here are a couple of projects I have worked on thus far.</p>
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "fadeIn" : ""}>
+                  {/* Section heading and description */}
+                  <h2>Projects</h2>
+                  <p>Here are a couple of projects I have worked on thus far.</p>
   
-                <Tab.Content className={isVisible ? "slideInUp" : ""}>
-                  <Tab.Pane eventKey="first">
-                    <Row>
-                      {projects.map((project, index) => (
-                        <Col md={4} key={index}>
-                          <ProjectCard {...project} />
-                        </Col>
-                      ))}
-                    </Row>
-                  </Tab.Pane>
-                </Tab.Content>
-              </div>}
-          </TrackVisibility>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-  
+                  <Tab.Content className={isVisible ? "slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      {/* Render project cards */}
+                      <Row>
+                        {projects.map((project, index) => (
+                          <Col md={4} key={index}>
+                            <ProjectCard {...project} />
+                          </Col>
+                        ))}
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
