0d6d7fb3208ada36c24cb23996a00b0614c02ff9
Juan Viljoen <juangaljoen@gmail.com>
2023-08-11 15:12:11 +0200

    visitor count

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
-export const Footer = () => {
-  const [views, setViews] = useState(0);
+// export const Footer = () => {
+//   const [views, setViews] = useState(0);
 
-  useEffect(() => {
-    fetch(process.env.REACT_APP_API_URL,
-      {method: 'GET'}) 
-      .then(response => response.json())
-      .then(data => setViews(data));
-  }, []);
+//   useEffect(() => {
+//     fetch(process.env.REACT_APP_API_URL,
+//       {method: 'GET'}) 
+//       .then(response => response.json())
+//       .then(data => setViews(data));
+//   }, []);
 
-  return (
-    <div>
-    <i class="fa fa-user medium-icon"></i>
-    <span class="counter-title"> Viewer COUNT </span>
-     <span class="timer counter alt-font appear" data-to={views} data-speed="7000">{views}</span>
+//   return (
+//     <div>
+//     <i class="fa fa-user medium-icon"></i>
+//     <span class="counter-title"> Visitor count </span>
+//      <span class="timer counter alt-font appear" data-to={views} data-speed="7000">{views}</span>
    
-</div>
+// </div>
     
-  );
-};
+//   );
+// };
 
 
