730e1f991789c96f76e267018cb915c9a765883e
Juan Viljoen <juangaljoen@gmail.com>
2023-08-14 10:30:33 +0200

    edit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,7 +9,6 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
 
-// import Projects from "./components/Projects";
 
 function App() {
   useEffect(() => {
