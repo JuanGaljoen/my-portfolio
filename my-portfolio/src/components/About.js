20131a4d972d1e9f4a8f486b26ae93286b911213
JuanGaljoen <juangaljoen@gmail.com>
2024-06-24 20:51:08 +0200

    Updated About

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -11,8 +11,8 @@ export const About = () => {
             <div className="about" id="aboutme" xs={12} md={6} xl={7}>
                 <h2>About me</h2>
                 <p>
-                    I am thrilled to take my first step into the world of Cloud Engineering, driven by my passion for cutting-edge technologies and their profound impact on businesses and individuals. While my background is in fine jewellery design,
-                    I want to change my career path for future growth potential and new challenges. As an aspiring Cloud Engineer, I find immense joy in problem-solving and have a relentless drive to learn and grow in this dynamic field. <br></br><br></br>
+                    I am driven by a passion for cutting-edge technologies and problem-solving. My hands-on experience with software development, combined with a strong foundation in cloud computing principles, enables me to contribute to scalable, secure, and reliable cloud infrastructures. I eagerly seek opportunities to collaborate with talented teams and support organizations in achieving their goals.
+                    <br></br><br></br>
                     <img className="background-about-image" src={"https://i.pinimg.com/564x/81/8f/4a/818f4a120483279ef50b769acf8188a6.jpg"} alt="astronaut" xs={12} md={6} xl={7}></img>
                     In my spare time I'm either working on an old Vespa, cycling, taking my dog for a hike/fishing trip or spending time with friends. I am very passionate about music, classic cars and motorbikes.
                 </p>
