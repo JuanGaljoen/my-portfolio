be33ac829e6cb90d12ec1d59f7e10c38ab43c14d
JuanGaljoen <juangaljoen@gmail.com>
2024-09-26 21:09:23 +0200

    removed section

--- my-portfolio/src/components/Banner.js
+++ my-portfolio/src/components/Banner.js
@@ -47,21 +47,19 @@ export const Banner = () => {
   }
 
   return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <span className="tagline">Welcome to my Portfolio!</span>
-                  <h1 className="name-heading">{`Hi! I'm Juan,`} <span className="" dataPeriod="1000" data-rotate='[ " Full Stack Web Developer", "Entrepreneur", "Jeweller" ]'><span className="wrap">{text}</span></span></h1>
-                  <p className="intro-paragraph">I am an enthusiastic and ambitious developer with experience in full-stack development, working with React and C# .NET and AWS. My professional journey is marked by a strong sense of pride and ownership in my work, understanding its critical impact on the overall success of the business. As a critical thinker and problem solver, I excel in fast-paced, agile environments that prioritize continuous learning and improvement. My commitment to delivering high-quality solutions drives meto stay at the forefront of technology and development practices.</p>
-                </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <Container className="banner" id="home">
+      <Row className="aligh-items-center">
+        <Col xs={12} md={6} xl={7}>
+          <TrackVisibility>
+            {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio!</span>
+                <h1 className="name-heading">{`Hi! I'm Juan,`} <span className="" dataPeriod="1000" data-rotate='[ " Full Stack Web Developer", "Entrepreneur", "Jeweller" ]'><span className="wrap">{text}</span></span></h1>
+                <p className="intro-paragraph">I am an enthusiastic and ambitious developer with experience in full-stack development, working with React and C# .NET and AWS. My professional journey is marked by a strong sense of pride and ownership in my work, understanding its critical impact on the overall success of the business. As a critical thinker and problem solver, I excel in fast-paced, agile environments that prioritize continuous learning and improvement. My commitment to delivering high-quality solutions drives meto stay at the forefront of technology and development practices.</p>
+              </div>}
+          </TrackVisibility>
+        </Col>
+      </Row>
+    </Container>
   )
 }
