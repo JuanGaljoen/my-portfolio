8c22362580962d1100bf070c9aa2154129f5dcec
JuanGaljoen <juangaljoen@gmail.com>
2024-10-17 11:20:21 +0200

    Updated Banner Background

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -1,13 +1,23 @@
 import { TypeAnimation } from "react-type-animation";
 import { Container, Row, Col } from "react-bootstrap";
+import clouds from '../assets/images/clouds.mp4'
 
 export const Home = () => {
   const sectionName = "Juan Viljoen";
   return (
-    <Container className="banner" id="home">
+    <Container className="banner" id="home" style={{ position: "relative", overflow: "hidden" }}>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: -1 }}
+      >
+        <source src={clouds} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <Row className="align-items-center">
         <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-        <h1 className="section-title" style={{ color: "black" }}>
+        <h1 className="section-title" style={{ color: "white" }}>
           <span className="text-grey" style={{ textAlign: "center" }}>
             {sectionName}
           </span>
@@ -24,7 +34,7 @@ export const Home = () => {
           wrapper="span"
           repeat={Infinity}
           cursor={true}
-          className="wrap" // Add your class name here
+          className="wrap"
         />
       </Row>
     </Container>
