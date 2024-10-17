380664cc43a4ceb8f29dd9d7af5018e140b4964d
JuanGaljoen <juangaljoen@gmail.com>
2024-10-17 14:22:39 +0200

    corrected typo

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -5,7 +5,7 @@ import vaLogo from '../assets/images/vamilling.png'
 
 const projects = [
   { id: 3, name: 'VA Milling Console', description: 'Developed a comprehensive business management system tailored for Agri-Manufacturing for our client, VA-Milling. It streamlines key operations including accounting, sales, production, and logistics. The system integrates with the Quickbooks API for seamless accounting processes. The backend is built in Rust and the frontend in Flutter.', image: vaLogo },
-  { id: 1, name: 'Fen-X External Data Adapter', description: 'Developed an external data adapter for Fen-X to intergrate Dunn and Bradstreets Direct+ API. Utilized .NET and AWS technologies to enable a seamless data exhange. We used Pulumi for IaC. ', image: fenLogo },
+  { id: 1, name: 'Fen-X External Data Adapter', description: 'Developed an external data adapter for Fen-X to intergrate Dunn and Bradstreets Direct+ API. Utilized .NET and AWS technologies to enable a seamless data exhange. Implemented IaC using Pulumi. ', image: fenLogo },
   { id: 2, name: 'Fen-X UI Project', description: 'Contributed to a Fenergo UI team where we worked on the core product, Fen-X, where we added new features to the user interface. Leveraged React and TypeScript to implement UI enhancements.', image: fenLogo },
 ];
 
